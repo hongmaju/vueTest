c52c52a7ecb1b4bafd723c00b5676b2181ed30fb
kk <517412663@qq.com>
2017-04-17 22:53:25 +0800

    vue中的filters的用法 vue的双向绑定

--- js/cart.js
+++ js/cart.js
@@ -1,7 +1,7 @@
 /**
  * Created by kk on 2017/4/16.
  */
-var vm =new Vue({
+new Vue({
     el:"#app",
     data:{
         // title:"hello vue"
@@ -9,21 +9,48 @@ var vm =new Vue({
         productList:[]
     },
     filters:{
-
+formatMoney:function (value) {
+    return "￥"+value.toFixed(2)
+}
     },
     mounted:function () {
         //类似于jquery中的ready方法
-        this.cartView();
+        this.$nextTick(function () {
+            this.cartView();
+        })
+
     },
     methods:{
         cartView:function () {
     // this.title="Vue hello"
-            var _this=this;
-            this.$http.get("data/cart.json",{"id":123}).then(function (res) {
-                _this.productList=res.body.result. productList;
-                _this.totalMoney=res.body.result.totalMoney;
+            //var _this=this;
+            // this.$http.get("data/cart.json",{"id":123}).then(function (res) {
+            //     _this.productList=res.body.result. productList;
+            //     _this.totalMoney=res.body.result.totalMoney;
+            // });
+//            ES6语法
+            let _this=this;
+            this.$http.get("data/cart.json",{"id":123}).then(res=> {
+                this.productList=res.body.result. productList;
+            this.totalMoney=res.body.result.totalMoney;
             });
-}
+},
+        changeMoney:function (product,way) {
+            if(way>0)
+            {
+                product.productQuentity++;
+            }
+            else{
+                product.productQuentity--;
+                if(product.productQuentity<1){
+                    product.productQuentity=1;
+                }
+            }
+
+        }
     }
 
+});
+Vue.filter("money",function (value,type) {
+    return "￥"+value.toFixed(2)+type;
 });
